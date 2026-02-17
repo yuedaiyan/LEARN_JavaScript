cd1c418800d930654342e0cfff5c112e749e4b0f
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-02-18 00:24:27 +0800

    Generate HTML for the order summary

---
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -0,0 +1,99 @@
+// 导入商品清单
+import { products } from "../data/products.js";
+// 导入购物车列表
+import { cart } from "../data/cart.js";
+let cartSummaryHTML;
+cart.forEach((cartItem) => {
+    let matchingProduct;
+    // 根据 cartItem 查找出完整项目
+    products.forEach((product) => {
+        if (product.id === cartItem.productId) {
+            matchingProduct = product;
+        }
+    });
+
+    // 生成 HTML
+    cartSummaryHTML += `
+          <div class="cart-item-container">
+            <div class="delivery-date">
+              Delivery date: Tuesday, June 21
+            </div>
+
+            <div class="cart-item-details-grid">
+              <img class="product-image"
+                src="${matchingProduct.image}">
+
+              <div class="cart-item-details">
+                <div class="product-name">
+                  ${matchingProduct.name}
+                </div>
+                <div class="product-price">
+                  $${(matchingProduct.priceCents / 100).toFixed(2)}
+                </div>
+                <div class="product-quantity">
+                  <span>
+                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                  </span>
+                  <span class="update-quantity-link link-primary">
+                    Update
+                  </span>
+                  <span class="delete-quantity-link link-primary">
+                    Delete
+                  </span>
+                </div>
+              </div>
+
+              <div class="delivery-options">
+                <div class="delivery-options-title">
+                  Choose a delivery option:
+                </div>
+                <div class="delivery-option">
+                  <input type="radio" checked
+                    class="delivery-option-input"
+                    name="delivery-option-1">
+                  <div>
+                    <div class="delivery-option-date">
+                      Tuesday, June 21
+                    </div>
+                    <div class="delivery-option-price">
+                      FREE Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-1">
+                  <div>
+                    <div class="delivery-option-date">
+                      Wednesday, June 15
+                    </div>
+                    <div class="delivery-option-price">
+                      $4.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-1">
+                  <div>
+                    <div class="delivery-option-date">
+                      Monday, June 13
+                    </div>
+                    <div class="delivery-option-price">
+                      $9.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+`;
+});
+// console.log(cartSummaryHTML);
+
+// 将生成的HTMl拼接到主HTML DOM树上
+document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;

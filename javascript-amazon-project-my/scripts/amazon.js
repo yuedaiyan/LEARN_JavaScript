d088f120a3375b0b2d13d95fc9691895b3f7a797
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-02-17 12:51:16 +0800

    Amazon Project: lesson 13 half

--- javascript-amazon-project-my/scripts/amazon.js
+++ javascript-amazon-project-my/scripts/amazon.js
@@ -1,33 +1,3 @@
-// const products = [
-//     {
-//         image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-//         name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-//         rating: {
-//             stars: 4.5,
-//             count: 87,
-//         },
-//         priceCents: 1090,
-//     },
-//     {
-//         image: "images/products/intermediate-composite-basketball.jpg",
-//         name: "Intermediate Size Basketball",
-//         rating: {
-//             stars: 4,
-//             count: 127,
-//         },
-//         priceCents: 2095,
-//     },
-//     {
-//         image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-//         name: "Adults Plain Cotton T-Shirt - 2 Pack",
-//         rating: {
-//             stars: 4.5,
-//             count: 56,
-//         },
-//         priceCents: 799,
-//     },
-// ];
-
 let productsHTML = "";
 products.forEach((product) => {
     productsHTML += `
@@ -67,10 +37,22 @@ products.forEach((product) => {
                         Added
                     </div>
 
-                    <button class="add-to-cart-button button-primary">Add to Cart</button>
+                    <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">Add to Cart</button>
                 </div> 
     `;
 });
 console.log(productsHTML);
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
+
+document.querySelectorAll(".js-add-to-cart").forEach
+    ((button) => {
+    button.addEventListener("click", () => {
+        console.log(button.dataset.productName);
+        cart.push({
+            productName: button.dataset.productName,
+            quantity:1    
+        })
+        console.log(cart);
+    });
+});

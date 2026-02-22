90baa71c4dac5e9385ab859234d47f2dfce71312
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-02-23 01:35:15 +0800

    Amazon Project: 16 Remove default tests and add money tests

---
+++ javascript-amazon-project-main/tests-jasmine/moneyTest.js
@@ -0,0 +1,22 @@
+// Two Types of Test Cases
+// 1. Basic test cases
+// = tests if the code is working
+// 2. Edge cases
+// = test with values that are tricky
+
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("test suite: formatCurrency", () => {
+    it("converts cents into dollars", () => {
+        expect(formatCurrency(2095)).toEqual("20.95");
+    });
+    it("works with 0", () => {
+        expect(formatCurrency(0)).toEqual("0.00");
+    });
+    it("rounds up to the nearest cent", () => {
+        expect(formatCurrency(2000.5)).toEqual("20.01");
+    });
+    it("rounds down to the nearest cent", () => {
+        expect(formatCurrency(2000.4)).toEqual("20.00");
+    });
+});

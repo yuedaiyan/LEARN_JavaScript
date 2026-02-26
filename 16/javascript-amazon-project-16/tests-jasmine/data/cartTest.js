369f59e8fc6d20d536b6981d6c5840cd48fc4039
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-02-26 22:34:39 +0800

    Amazon Project: 16d

--- 16/javascript-amazon-project-16/tests-jasmine/data/cartTest.js
+++ 16/javascript-amazon-project-16/tests-jasmine/data/cartTest.js
@@ -49,6 +49,7 @@ describe("test suite: addToCart", () => {
         addToCart("9baab029f463f330bb33ed5676aa4dfd");
         expect(cart.length).toEqual(1);
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(localStorage.setItem).toHaveBeenCalledWith("cart", JSON.stringify([{ productId: "9baab029f463f330bb33ed5676aa4dfd", quantity: 1, deliveryOptionId: "3" }]));
         expect(cart[0].productId).toEqual("9baab029f463f330bb33ed5676aa4dfd");
         expect(cart[0].quantity).toEqual(1);
     });

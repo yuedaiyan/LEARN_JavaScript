cc982cb5626220fbecf8e45b96b87efb5da240b8
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-02-27 00:17:14 +0800

    Amazon Project: 16i

--- 16/javascript-amazon-project-16/tests-jasmine/data/cartTest.js
+++ 16/javascript-amazon-project-16/tests-jasmine/data/cartTest.js
@@ -2,11 +2,11 @@
 // Test each condition of an if-statement
 // 测试要求
 // 1.被测试函数中的每个if逻辑,都应测试
-import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage,removeFromCart } from "../../data/cart.js";
 
 // 测试 "加入购物车"
 describe("test suite: addToCart", () => {
-    // 提群准备:
+    // 提前准备:
     beforeEach(() => {
         // 劫持函数: localStorage.setItem → null
         spyOn(localStorage, "setItem");
@@ -56,3 +56,35 @@ describe("test suite: addToCart", () => {
         expect(cart[0].quantity).toEqual(1);
     });
 });
+
+// 测试: 删除购物车中的商品
+describe('test suit: removeFromCart (删除购物车中的商品)', () => {
+    // 提前准备:
+    beforeEach(() => {
+        // 劫持函数: localStorage.getItem → 提前放入两个商品
+        spyOn(localStorage, "getItem").and.callFake(() => {
+            return JSON.stringify([{ productId: "a7ad3bba44ce67fcd915e5c9dc4bd455", quantity: 1, deliveryOptionId: "1" },{ productId: "9baab029f463f330bb33ed5676aa4dfd", quantity: 1, deliveryOptionId: "1" }]);
+        });
+        // 将cart数组直接提出到栈中,模拟cart.js中,cart数组也一直在栈中的效果
+        loadFromStorage();
+        // 劫持函数: 获取用户选择的数量
+        spyOn(document, "querySelector").and.callFake(() => {
+            return { value: "1" };
+        });
+        // 劫持函数: localStorage.setItem → null
+        spyOn(localStorage, "setItem");
+    });
+
+    it("remove a productId that is in the cart.", () => {
+        removeFromCart("a7ad3bba44ce67fcd915e5c9dc4bd455");
+        expect(cart.length).toEqual(1)
+        expect(localStorage.setItem).toHaveBeenCalledWith("cart", JSON.stringify([{ productId: "9baab029f463f330bb33ed5676aa4dfd", quantity: 1, deliveryOptionId: "1" }]));
+    });
+
+    it("remove a productId that's not in the cart (does nothing).", () => {
+        removeFromCart("15b6fc6f-327a-4ec4-896f-486349e85a3d");
+        expect(cart.length).toEqual(2)
+        expect(localStorage.setItem).toHaveBeenCalledWith("cart", JSON.stringify([{ productId: "a7ad3bba44ce67fcd915e5c9dc4bd455", quantity: 1, deliveryOptionId: "1" },{ productId: "9baab029f463f330bb33ed5676aa4dfd", quantity: 1, deliveryOptionId: "1" }]));
+        
+    })
+})

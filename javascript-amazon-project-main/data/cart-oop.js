1d23c7847c3d56779d187f7f9806a0bff54ebbd2
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-02-27 16:26:39 +0800

    Amazon Project: 17 Create class version of cart

--- javascript-amazon-project-main/data/cart-oop.js
+++ javascript-amazon-project-main/data/cart-oop.js
@@ -135,12 +135,12 @@ const businessCart = Cart("cart-business");
 cart.loadFromStorage();
 businessCart.loadFromStorage();
 
+
 cart.addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
 cart.addToCart("15b6fc6f-327a-4ec4-896f-486349e85a3d");
 businessCart.addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
 businessCart.addToCart("15b6fc6f-327a-4ec4-896f-486349e85a3d");
 
-// cart.addToCart("83d4ca15-0f35-48f5-b7a3-1ea210004f2e");
 
 console.log(cart);
 console.log(businessCart);

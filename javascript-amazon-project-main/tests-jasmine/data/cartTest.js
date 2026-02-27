33356286e0440989af54d6d1cf8556f3a9452099
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-02-27 14:27:22 +0800

    Amazon Project: 17 小错误修复

--- javascript-amazon-project-main/tests-jasmine/data/cartTest.js
+++ javascript-amazon-project-main/tests-jasmine/data/cartTest.js
@@ -97,7 +97,7 @@ describe("test suit: removeFromCart (删除购物车中的商品)", () => {
     });
 });
 
-describe("test suit: updateDeliveryOption() (更改购物车中特定商品额数量))", () => {
+describe("test suit: updateDeliveryOption() (更改购物车中特定商品额数量)", () => {
     // 提前准备:
     const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
     const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";

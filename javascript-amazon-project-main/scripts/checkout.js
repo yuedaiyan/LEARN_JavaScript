5ad4b82c115e6a4e8af28685e4ca8e33f67234d7
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-02-27 15:25:26 +0800

    Amazon Project: 17 Create oop version of cart

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -2,6 +2,8 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 // 导入: 渲染右侧总金额计算函数
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+// 导入: cart-oop.js
+import '../data/cart-oop.js'
 
 // 执行: 渲染左侧商品菜单函数
 renderOrderSummary();

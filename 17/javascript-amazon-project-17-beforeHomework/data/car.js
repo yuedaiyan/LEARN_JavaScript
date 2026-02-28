2d4fea668012ee8fee0ec5a34c435c51ebabc9c8
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-02-28 13:58:48 +0800

    Amazon Project: 17 Homework 17b

--- 17/javascript-amazon-project-17-beforeHomework/data/car.js
+++ 17/javascript-amazon-project-17-beforeHomework/data/car.js
@@ -5,10 +5,17 @@ class Car {
         this.brand = brand;
         this.model = model;
     }
+
+    displayInfo() {
+        console.log(`${this.brand} ${this.model}`);
+    }
 }
 
 const car_1=new Car("Toyota","Corolla")
 const car_2=new Car("Tesla","Model 3")
 
-console.log(car_1);
-console.log(car_2);
+// console.log(car_1);
+// console.log(car_2);
+
+car_1.displayInfo()
+car_2.displayInfo()

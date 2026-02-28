ca98ff05381ea9edd105c60eb205f4a2eb893b90
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-02-28 14:16:16 +0800

    Amazon Project: 17 Homework 17d

--- 17/javascript-amazon-project-17-beforeHomework/data/car.js
+++ 17/javascript-amazon-project-17-beforeHomework/data/car.js
@@ -2,18 +2,25 @@ class Car {
     brand;
     model;
     speed;
+    #isTrunkOpen;
+
     constructor(brand, model) {
         this.brand = brand;
         this.model = model;
         this.speed = 0;
+        this.#isTrunkOpen=false
     }
 
     displayInfo() {
-        console.log(`${this.brand} ${this.model}, Speed:  ${this.speed} km/h`);
+        console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h, Trunk: ${this.#isTrunkOpen}`);
     }
     go() {
-        this.speed += 5;
-        this.limitedSpeed();
+        if (this.#isTrunkOpen === false) {
+            this.speed += 5;
+            this.limitedSpeed();
+        } else {
+            return;
+        }
     }
     brake() {
         this.speed -= 5;
@@ -28,30 +35,27 @@ class Car {
             this.speed = 0;
         }
     }
+
+    openTrunk() {
+        // 只有当 speed=0 的时候,才可以 open 后备箱
+        if (this.speed === 0) {
+            this.#isTrunkOpen = true;
+        } else {
+            return;
+        }
+    }
+
+    closeTrunk() {
+        this.#isTrunkOpen = close;
+    }
 }
 
 const car_1 = new Car("Toyota", "Corolla");
 const car_2 = new Car("Tesla", "Model 3");
+// const car_3 = new Car("McLaren", "F1");
 
 // console.log(car_1);
 // console.log(car_2);
-
-car_1.displayInfo();
-car_2.displayInfo();
-
-car_1.go();
-car_1.go();
-car_1.go();
-car_1.go();
-car_1.go();
-car_1.go();
-car_1.go();
-
-car_2.go();
-car_2.brake();
-car_2.brake();
-car_2.brake();
-car_2.brake();
 
 car_1.displayInfo();
 car_2.displayInfo();

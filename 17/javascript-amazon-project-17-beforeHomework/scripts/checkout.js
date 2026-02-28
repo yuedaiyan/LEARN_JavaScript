a6945ac77dfc83a0fb928c75d7776a99f6c4ca60
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-02-28 14:37:02 +0800

    Amazon Project: 17 Homework 17g

--- 17/javascript-amazon-project-17-beforeHomework/scripts/checkout.js
+++ 17/javascript-amazon-project-17-beforeHomework/scripts/checkout.js
@@ -10,7 +10,7 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js'
 
 // 导入: 17a 作业
-import '../data/car.js'
+// import '../data/car.js'
 
 // 执行: 渲染左侧商品菜单函数
 renderOrderSummary();

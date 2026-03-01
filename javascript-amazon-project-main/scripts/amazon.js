8a7324d10cf577994a1abcc59e9874e14c3faeba
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-03-02 03:14:09 +0800

    Load products from the backend

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,18 +1,23 @@
 // 导入商品清单
-import { products } from "../data/products.js";
+import { products, loadProducts } from "../data/products.js";
 // 导入购物车列表
 import { cart } from "../data/cart-class.js";
 // 导入购物车内商品总数计算函数
 // import { calculateCartQuantity } from "../data/cart-class.js";
 
-// 初始化购物车列表
-let productsHTML = "";
-// 初始化pressId(用户记录显示的绿色icon Id)
-const pressId = {};
+// 一进入页面,立刻开始请求
+loadProducts(renderProductsGrid);
 
-// 依据js文件:data/product.js中的list,逐条生成HTML
-products.forEach((product) => {
-    productsHTML += `
+renderProductsGrid()
+function renderProductsGrid() {
+    // 初始化购物车列表
+    let productsHTML = "";
+    // 初始化pressId(用户记录显示的绿色icon Id)
+    const pressId = {};
+
+    // 依据js文件:data/product.js中的list,逐条生成HTML
+    products.forEach((product) => {
+        productsHTML += `
                    <div class="product-container">
                     <div class="product-image-container">
                         <img class="product-image" src="${product.image}" />
@@ -55,44 +60,42 @@ products.forEach((product) => {
                     <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">Add to Cart</button>
                 </div> 
     `;
-});
-
-// console 打印页面中所有商品条目的HTML
-// console.log(productsHTML);
-
-// 将生成的商品条目HTML,加入到.js-products-grid中,以供渲染
-document.querySelector(".js-products-grid").innerHTML = productsHTML;
-
-// 初始化购物车内商品数量
-let cartQuantity = 0;
-// 使用实际的cart数据,更新购物车数量
-cartQuantity = cart.calculateCartQuantity(cart);
-// 刷新屏幕
-document.querySelector(".js-cart-quantity").innerHTML = cartQuantity || "";
-
-// Add 绿色提示
-function showAddedToCartIcon(productId) {
-    // 提前取消上一轮遗留的显示效果(如果显示效果存在的话(即便不存在,setTimeOut也不会报错))
-    clearTimeout(pressId[productId]);
-    // 点击 Add 按钮之后,上面出现绿色的提示
-    const addedToCartEl = document.querySelector(`.js-added-to-cart-${productId}`);
-    addedToCartEl.classList.add("added-to-cart-pressed");
-    pressId[productId] = setTimeout(() => {
-        addedToCartEl.classList.remove("added-to-cart-pressed");
-        delete pressId[productId];
-    }, 1500);
-}
-
-// 给每个按钮添加监听器 → 添加到购物车列表
-document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-    button.addEventListener("click", () => {
-        const { productId } = button.dataset;
-        cart.addToCart(productId);
-
-        // 刷新屏幕:修改屏幕右上角购物车商品数量
-        // 处理0的问题
-        document.querySelector(".js-cart-quantity").innerHTML = cart.calculateCartQuantity(cart) || "";
-
-        showAddedToCartIcon(productId);
     });
-});
+
+    // 将生成的商品条目HTML,加入到.js-products-grid中,以供渲染
+    document.querySelector(".js-products-grid").innerHTML = productsHTML;
+
+    // 初始化购物车内商品数量
+    let cartQuantity = 0;
+    // 使用实际的cart数据,更新购物车数量
+    cartQuantity = cart.calculateCartQuantity(cart);
+    // 刷新屏幕
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity || "";
+
+    // Add 绿色提示
+    function showAddedToCartIcon(productId) {
+        // 提前取消上一轮遗留的显示效果(如果显示效果存在的话(即便不存在,setTimeOut也不会报错))
+        clearTimeout(pressId[productId]);
+        // 点击 Add 按钮之后,上面出现绿色的提示
+        const addedToCartEl = document.querySelector(`.js-added-to-cart-${productId}`);
+        addedToCartEl.classList.add("added-to-cart-pressed");
+        pressId[productId] = setTimeout(() => {
+            addedToCartEl.classList.remove("added-to-cart-pressed");
+            delete pressId[productId];
+        }, 1500);
+    }
+
+    // 给每个按钮添加监听器 → 添加到购物车列表
+    document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+        button.addEventListener("click", () => {
+            const { productId } = button.dataset;
+            cart.addToCart(productId);
+
+            // 刷新屏幕:修改屏幕右上角购物车商品数量
+            // 处理0的问题
+            document.querySelector(".js-cart-quantity").innerHTML = cart.calculateCartQuantity(cart) || "";
+
+            showAddedToCartIcon(productId);
+        });
+    });
+}
--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -2,7 +2,16 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 // 导入: 渲染右侧总金额计算函数
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+// 导入: 后端购物车
+import { products, loadProducts } from "../data/products.js";
 
+// 在页面中载入 products
+loadProducts(() => {
+    // 执行: 渲染左侧商品菜单函数
+    renderOrderSummary();
+    // 执行: 渲染右侧总金额计算函数
+    renderPaymentSummary();
+});
 // 导入: cart-oop.js
 // import '../data/cart-oop.js'
 
@@ -12,9 +21,10 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // 导入: 17a 作业
 // import '../data/car.js'
 
-import '../data/backend-practice.js'
+// 18: learn backend
+// import '../data/backend-practice.js'
 
-// 执行: 渲染左侧商品菜单函数
-renderOrderSummary();
-// 执行: 渲染右侧总金额计算函数
-renderPaymentSummary();
+// // 执行: 渲染左侧商品菜单函数
+// renderOrderSummary();
+// // 执行: 渲染右侧总金额计算函数
+// renderPaymentSummary();

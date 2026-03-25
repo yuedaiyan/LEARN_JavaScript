5420562735c0183c1e891e98f28aeba42377a0b8
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-03-26 03:25:57 +0800

    delet useless folder

--- javascript-amazon-project-main-cartNotUseClass/scripts/utils/money.js
+++
@@ -1,7 +0,0 @@
-export function formatCurrency(priceCents) {
-    return (Math.round(priceCents) / 100).toFixed(2);
-    // return (priceCents / 100).toFixed(2);
-}
-
-// console.log(formatCurrency(-2000));
-// -20.00
